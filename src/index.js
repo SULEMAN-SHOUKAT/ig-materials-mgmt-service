9c2ddc2e3281ed7453175c674cbc62f71a26f823
suleman <sulemanshoukaat@gmail.com>
2023-03-29 16:45:45 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const config = require("config");
+const cors = require("cors");
 
 const { connectDb } = require("./models");
 const router = require("./api/http/index");
 const { logHttpRequest } = require("./utils");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(config.baseRoute, logHttpRequest, router);
