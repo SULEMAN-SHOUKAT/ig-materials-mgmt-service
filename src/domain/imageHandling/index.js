9cb21faa5ec1240a32479450bba93d1186c716ea
suleman <sulemanshoukaat@gmail.com>
2023-04-02 15:17:03 +0200

    Add images conversion

--- src/domain/imageHandling/index.js
+++ src/domain/imageHandling/index.js
@@ -15,8 +15,31 @@ const saveImage = async (img, mode) => {
 const deleteByIds = (imageIds) =>
   ImagesModel.deleteMany({ _id: { $in: imageIds } });
 
-const getByIds = (imageIds) =>
-  ImagesModel.find({ _id: { $in: imageIds } }).sort({ createdAt: -1 });
+const getByIds = async (imageIds) => {
+  let images = await ImagesModel.find({ _id: { $in: imageIds } }).sort({
+    createdAt: -1,
+  });
+  let formattedImages =
+    images.length > 0
+      ? images.map((img) => {
+          const buffer = Buffer.from(img.data);
+          const base64 = buffer.toString("base64");
+          const dataUrl = `data:image/jpeg;base64,${base64}`;
+          return {
+            _id: img._id,
+            name: img.name,
+            mode: img.mode,
+            type: img.type,
+            size: img.size,
+            createdAt: img.createdAt,
+            updatedAt: img.updatedAt,
+            data: dataUrl,
+          };
+        })
+      : [];
+
+  return formattedImages;
+};
 
 module.exports = {
   saveImage,
